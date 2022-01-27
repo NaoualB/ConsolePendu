cd4fcb8448e08527f8b64b17400fcd7579e81f0c
Naoual BECHIR <n.bechir@hotmail.fr>
2022-01-27 15:07:02 +0100

    Ajoutez des fichiers projet.

---
+++ ConsolePendu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePendu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pendu = new GameInstance();
+            pendu.Play();
+        }
+    }
+}
---
+++ ConsolePendu/Word.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePendu
+    {
+        public class Word
+        {
+            public string Text { get; set; }
+            public int Length { get; }
+
+            public Word(string text)
+            {
+                Text = text.ToUpper();
+                Length = Text.Length;
+            }
+
+            public int GetIndexOf(char letter)
+            {
+                return Text.IndexOf(letter);
+            }
+        }
+    }
+
